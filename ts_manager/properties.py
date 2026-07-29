4c46369ba124068e91125d007a4c51d7385bd4f8
SerhiiTrystan <salingartist@gmail.com>
2026-07-29 22:22:25 +0200

    add ts_manager

--- ts_manager/properties.py
+++ ts_manager/properties.py
@@ -1,3 +1,31 @@
 import bpy
 
-class TSM_PTE_(bpy.types.PropertyGroup):
+
+class SPM_ProjectItem(bpy.types.PropertyGroup):
+    name: bpy.props.StringProperty(name="Project Name")
+    folder_path: bpy.props.StringProperty(name="Folder Path", subtype='DIR_PATH')
+    status: bpy.props.StringProperty(name="Status", default="active")
+    pinned: bpy.props.BoolProperty(name="Pinned", default=False)
+
+
+def register_properties():
+    bpy.utils.register_class(SPM_ProjectItem)
+
+    bpy.types.Scene.spm_projects = bpy.props.CollectionProperty(
+        type=SPM_ProjectItem
+    )
+
+    bpy.types.Scene.spm_project_index = bpy.props.IntProperty(default=0)
+
+    bpy.types.Scene.spm_show_archived = bpy.props.BoolProperty(
+        name="Show Archived",
+        default=False
+    )
+
+
+def unregister_properties():
+    del bpy.types.Scene.spm_projects
+    del bpy.types.Scene.spm_project_index
+    del bpy.types.Scene.spm_show_archived
+
+    bpy.utils.unregister_class(SPM_ProjectItem)
